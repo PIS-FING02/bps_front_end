70ca66b87b9a2495b6a8027e721f52b4be5eadbe
9uzman7 <gperera30@fing.edu.uy>
2016-09-10 03:57:09 -0300

    Se agrego logica

---
+++ SARP_Logica/src/com/logica/Main.java
@@ -0,0 +1,12 @@
+package com.logica;
+
+import com.controladores.ControladorREST;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ControladorREST c = new ControladorREST();
+		c.Consumir();
+	}
+
+}
