a83fdcf13d61e1a99ff6a6000a4c5883e2a736f1
Guillermo Coduri <guillermo.coduri@mercadolibre.com>
2016-10-07 22:17:58 -0300

    pepe

--- SARPTest/src/main.java
+++ SARPTest/src/main.java
@@ -1,8 +1,42 @@
+import org.jboss.resteasy.client.ClientResponse;
+
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			
+			Client client = Client.create();
+
+			WebResource webResource = client
+			   .resource("http://52.52.100.160:8080/SARPService/adminService/puesto");
+
+			String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
+
+			ClientResponse response = webResource.type("application/json")
+			   .post(ClientResponse.class, input);
+
+			if (response.getStatus() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+				     + response.getStatus());
+			}
+
+			System.out.println("Output from Server .... \n");
+			String output = (String) response.getEntity(String.class);
+			System.out.println(output);
+
+
+
+			
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
 
 	}
 
